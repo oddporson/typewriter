63f599b70d1d9bcf2d5fb777ceb3046df79b352d
Porson <porsonlee@gmail.com>
2019-12-03 22:20:19 +0000

    changed s to typed

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,8 @@
 const sentence = "hello there from lighthouse labs\n";
-const typewriter = (s) => {
-for (let i = 0; i < s.length; i++) {
+const typewriter = (typed) => {
+for (let i = 0; i < typed.length; i++) {
   setTimeout(() => {
-    process.stdout.write(s[i]); // print the char here
+    process.stdout.write(typed[i]); // print the char here
   },i * 100) // <= 1s delay to make it noticeable. Can dial it down later.
   }
 }
