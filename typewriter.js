7a2715ae57ece5a40052bcea9f839b043dd2a66f
Porson Lee <52018594+oddporson@users.noreply.github.com>
2020-02-27 02:32:23 -0800

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -6,12 +6,4 @@ for (let i = 0; i < typed.length; i++) {
   },i * 100) // <= 1s delay to make it noticeable. Can dial it down later.
   }
 }
-typewriter(sentence);
-
-// const sentence = "hello there from lighthouse labs\n";
-// for (const [i, char] of sentence.split("").entries()) {
-//   //process.stdout.write(char);
-//   setTimeout(() => {
-//     process.stdout.write(char); // print the char here
-//   },i *  500) // <= 1s delay to make it noticeable. Can dial it down later.
-// }
+sentence;
